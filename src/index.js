026edc6825067f7a868e442a85a581b819506f0b
Betsycampos <cbetsydalia@gmail.com>
2018-11-20 15:07:58 -0500

    cifrado de  una letra

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function loadData() {
+  const palabra = document.getElementById('altern1').value;
+  const offset = document.getElementById('altern2').value;
+  const ascii = palabra.charCodeAt(0); // codigo ascii
+
+  const resultado = String.fromCharCode((ascii - 65 + offset )%26 + 65)
+  //const resultado = String.fromCharCode((ascii - 65 - offset )%26 + 65)
+
+
+  document.getElementById('prueba').innerHTML=resultado;
+
+}
