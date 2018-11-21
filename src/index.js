35724d7406341b98519a2d1726a02d3329a5278b
Betsycampos <cbetsydalia@gmail.com>
2018-11-21 12:11:47 -0500

    Decifrado de una letra y color de fondo de pantalla

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function loadData() {
+function loadData1() {
   const palabra = document.getElementById('altern1').value;
   const offset = document.getElementById('altern2').value;
   const ascii = palabra.charCodeAt(0); // codigo ascii
@@ -7,6 +7,16 @@ function loadData() {
   //const resultado = String.fromCharCode((ascii - 65 - offset )%26 + 65)
 
 
-  document.getElementById('prueba').innerHTML=resultado;
+        document.getElementById('PRUEBA1').innerHTML=resultado;
+}
 
+
+function loadData2() {
+  const palabra1 = document.getElementById('altern1').value;
+  const offset1 = document.getElementById('altern2').value;
+  const ascii1 = palabra1.charCodeAt(0); // codigo ascii
+
+  const resultado1 = String.fromCharCode((ascii1 - 65 - offset1 )%26 + 65);
+
+        document.getElementById('PRUEBA2').innerHTML=resultado1;
 }
