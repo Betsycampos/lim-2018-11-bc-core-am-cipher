5df4e4227808f160f0ab624ff6c6b75f3b2b6ab7
Betsycampos <cbetsydalia@gmail.com>
2018-11-22 13:16:24 -0500

    Añade cambio de funcion

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-function loadData1() { 
+//function loadData1() {
+  const loadData1 = () => {
   const palabra = document.getElementById('altern1').value;
   const offset = parseInt(document.getElementById('altern2').value);
   const ascii = palabra.charCodeAt(); // codigo ascii
   //let resultado="";
   resultado1 = String.fromCharCode((ascii - 65 + offset )%26 + 65);
   //const resultado = String.fromCharCode((ascii - 65 - offset )%26 + 65)
-
+const arr = []
+  for
   document.getElementById('PRUEBA1').innerHTML=resultado1;
 
 }
-function loadData2() {
+//function loadData2() {
+  const loadData2 = () => {
   const palabra1 = document.getElementById('altern1').value;
   const offset1 = parseInt(document.getElementById('altern2').value);
   const ascii1 = palabra1.charCodeAt(0); // codigo ascii
