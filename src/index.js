b0bd25aca8a29c4c4c2dd2a755123f48a80fd56c
Betsycampos <cbetsydalia@gmail.com>
2018-11-22 12:39:52 -0500

    Añade: cipher & decipher

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-function loadData1() {
+function loadData1() { 
   const palabra = document.getElementById('altern1').value;
-  const offset = document.getElementById('altern2').value;
-  const ascii = palabra.charCodeAt(0); // codigo ascii
-
-  const resultado = String.fromCharCode((ascii - 65 + offset )%26 + 65);
+  const offset = parseInt(document.getElementById('altern2').value);
+  const ascii = palabra.charCodeAt(); // codigo ascii
+  //let resultado="";
+  resultado1 = String.fromCharCode((ascii - 65 + offset )%26 + 65);
   //const resultado = String.fromCharCode((ascii - 65 - offset )%26 + 65)
 
+  document.getElementById('PRUEBA1').innerHTML=resultado1;
 
-        document.getElementById('PRUEBA1').innerHTML=resultado;
 }
-
-
 function loadData2() {
   const palabra1 = document.getElementById('altern1').value;
-  const offset1 = document.getElementById('altern2').value;
+  const offset1 = parseInt(document.getElementById('altern2').value);
   const ascii1 = palabra1.charCodeAt(0); // codigo ascii
 
-  const resultado1 = String.fromCharCode((ascii1 - 65 - offset1 )%26 + 65);
+  resultado2 = ((ascii1 - 65 - offset1 )%26 + 65);
+   if (resultado2 < 65) {
+     resultado2 += 26;
 
-        document.getElementById('PRUEBA2').innerHTML=resultado1;
+   }
+  let resultadoFinal =String.fromCharCode (resultado2);
+
+  document.getElementById('PRUEBA2').innerHTML=resultadoFinal;
 }
