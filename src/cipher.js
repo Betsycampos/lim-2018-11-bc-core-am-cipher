6d8b1b3087d59a6e5ca045a44faa17dd605b215d
Betsycampos <cbetsydalia@gmail.com>
2018-11-23 14:14:56 -0500

    Añade primer cambio en CSS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,6 @@
 window.cipher = {
-  // ...
+  //encode
+  //decode
 };
 //function loadData1() {
  const betsy = document.getElementById('cargar')
@@ -17,31 +18,6 @@ window.cipher = {
   document.getElementById('prueba1').innerHTML = msg;
 }
 betsy.addEventListener("click",loadData1);
-  //let resultado="";
-
-  //const resultado = String.fromCharCode((ascii - 65 - offset )%26 + 65)
-  /*
-//function loadData2() {
-  const loadData2 = () => {
-  const palabra1 = document.getElementById('altern1').value;
-  const offset1 = parseInt(document.getElementById('altern2').value);
-  const ascii1 = palabra1.charCodeAt(0); // codigo ascii
-
-  resultado2 = ((ascii1 - 65 - offset1 )%26 + 65);
-   if (resultado2 < 65) {
-     resultado2 += 26;
-
-   }
-  let resultadoFinal =String.fromCharCode (resultado2);
-
-  document.getElementById('PRUEBA2').innerHTML=resultadoFinal;
-
-
-}
-*/
-//const resultado = String.fromCharCode((ascii - 65 - offset )%26 + 65)
-
-//function loadData2() {
 
 
 
@@ -53,7 +29,7 @@ betsy.addEventListener("click",loadData1);
   for (let i = 0; i < palabra1.length; i++) {
   const antiguoAscii1=palabra1.toUpperCase().charCodeAt(i);
 
-  const ascii1 = (antiguoAscii1  + 65 - offset1 )%26 + 65; // codigo ascii
+  const ascii1 = (antiguoAscii1  + 65 -  offset1 )%26 + 65; // codigo ascii
 
    if (ascii1 < 65) {
      ascii1 += 26;
