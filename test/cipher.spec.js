57b346d11a9defca5c4aa5a2879b4d1235b6c266
Betsycampos <cbetsydalia@gmail.com>
2018-11-27 17:33:48 -0500

    Añadiendo test

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,7 +10,9 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
+      assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG")
+    });
   });
 
   describe('cipher.decode', () => {
@@ -19,7 +21,12 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      assert.equal(cipher.decode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG")
+    });
+    it('deberia retornar "ABC123" para "BCD123" con offset 1',()=>{
+      assert.equal(cipher.decode(1,"ABC123"),"BCD123")
+    } )
   });
 
   describe('cipher.createCipherWithOffset', () => {
