b09e32c040cafdbc51a9a57539d93d9594924d15
Betsycampos <cbetsydalia@gmail.com>
2018-11-27 17:03:57 -0500

    Añade new

--- src/cipher.js
+++ src/cipher.js
@@ -1,53 +1,36 @@
 window.cipher = {
-  encode: (offset, string) =>{
-  //decode
+
+  encode: (offset, string) => {
+
     let msg = "";
     for (let i = 0; i < string.length; i++) {
-     if(string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
-        let antiguoAscii = string[i].charCodeAt() ;
-        let ascii = (((antiguoAscii  - 65) + offset )%26) + 65; // codigo ascii
+      if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90) {
+        let antiguoAscii = string[i].charCodeAt();
+        let ascii = (((antiguoAscii - 65) + offset) % 26) + 65; // codigo ascii
         let resultado1 = String.fromCharCode(ascii);
         msg = msg + resultado1;
       }
-      else{
+      else {
         msg = msg + string[i];
       }
-  }
-
-return msg;
-},
-
-decode: (offset, string) =>{
-   let msg1="";
-   for (let i = 0; i < string.length; i++) {
-   let antiguoAscii1=string[i].charCodeAt();
-   let ascii1 = (((antiguoAscii1  + 65) -  offset )%26) + 65; // codigo ascii
-       if (ascii1 < 65) {
-       ascii1 += 26;
     }
-    let resultadoFinal =String.fromCharCode (ascii1);
-    msg1 = msg1 + resultadoFinal;
+    return msg;
+  },
+
+  decode: (offset, string) => {
+
+    let msg1 = "";
+    for (let i = 0; i < string.length; i++) {
+      if (string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90) {
+        let antiguoAscii1 = string[i].charCodeAt();
+        let ascii1 = (((antiguoAscii1 + 65) - offset) % 26) + 65; // codigo ascii
+        if (ascii1 < 65) {
+          ascii1 += 26;
+        }
+        let resultadoFinal = String.fromCharCode(ascii1);
+        msg1 = msg1 + resultadoFinal;
+      }
+    }
+    return msg1;
+  },
 }
-return msg1;
-},
- }
-
-
-/*
-
-  for (let i = 0; i < palabra1.length; i++) {
-  const antiguoAscii1=palabra1.toUpperCase().charCodeAt(i);
-
-  const ascii1 = (antiguoAscii1  + 65 -  offset1 )%26 + 65; // codigo ascii
-
-   if (ascii1 < 65) {
-     ascii1 += 26;
-
-   }
-  let resultadoFinal =String.fromCharCode (ascii1);
-  msg1 = msg1 + resultadoFinal;
-
-}
-
-document.getElementById('prueba2').innerHTML=msg1;
-*/
--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
 
 const texto = document.getElementById('texto1');
 const codigo = document.getElementById('codigo');
-
 const prueba1 = document.getElementById('prueba1');
 const prueba2 = document.getElementById('prueba2');
-
 const btnCifrar = document.getElementById('btn-cifrar');
 const btnDescifrar = document.getElementById('btn-descifrar');
 
 
 //Función cifrar
-cifrar = () =>{
+ const Cifrar = () =>{
   let numero = parseInt(codigo.value);
   let mensaje = texto.value.toUpperCase();
-  let resultado = cipher.encode(numero, mensaje);
-  prueba1.innerHTML= resultado;
-  //alert('hola');
+  let resultado1 = cipher.encode(numero, mensaje);
+  prueba1.innerHTML= resultado1;
+
 }
 //Función descifrar
-descifrar = () =>{
+ const Descifrar = () =>{
   let numero = parseInt(codigo.value);
   let mensaje= texto.value.toUpperCase();
-  let resultado1 = cipher.decode(numero, mensaje);
-  prueba2.innerHTML= resultado1
+  let resultado2 = cipher.decode(numero, mensaje);
+  prueba2.innerHTML= resultado2
 
 }
 
-btnCifrar.addEventListener('click', cifrar);
-btnDescifrar.addEventListener('click', descifrar);
+btnCifrar.addEventListener('click', Cifrar);
+btnDescifrar.addEventListener('click', Descifrar);
