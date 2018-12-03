a85b4c189b11406bd851359ae371fb2dc617efaa
Betsycampos <cbetsydalia@gmail.com>
2018-12-03 16:57:54 -0500

    Añade renovación en el css

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 
-const texto = document.getElementById("texto1");
+const texto = document.getElementById("texto");
 const codigo = document.getElementById("codigo");
 const prueba1 = document.getElementById("prueba1");
 //const prueba2 = document.getElementById("prueba2");
 const btnCifrar = document.getElementById("btn-cifrar");
 const btnDescifrar = document.getElementById("btn-descifrar");
-const buttonNext=document.getElementById("next");
-const mostrando1 =document.getElementById("hoja1");
-const ocultado1=document.getElementById("hoja2");
-const buttonReturn=document.getElementById("return");
+const buttonNext = document.getElementById("next");
+const mostrando1 = document.getElementById("hoja1");
+const ocultado1 = document.getElementById("hoja2");
+const buttonReturn = document.getElementById("return");
 
 //Función cifrar
- const Cifrar = () =>{
+const Cifrar = () => {
   let numero = parseInt(codigo.value);
   let mensaje = texto.value.toUpperCase();
   let resultado1 = cipher.encode(numero, mensaje);
-  prueba1.style.display="block";
-  prueba1.innerHTML= resultado1;
+  prueba1.style.display = "block";
+  prueba1.innerHTML = resultado1;
 
 }
 //Función descifrar
- const Descifrar = () =>{
+const Descifrar = () => {
   let numero = parseInt(codigo.value);
-  let mensaje= texto.value.toUpperCase();
+  let mensaje = texto.value.toUpperCase();
   let resultado2 = cipher.decode(numero, mensaje);
-  prueba1.innerHTML= resultado2;
+  prueba1.innerHTML = resultado2;
 
 }
-ocultado1.style.display="none";
+ocultado1.style.display = "none";
 btnCifrar.addEventListener("click", Cifrar);
 btnDescifrar.addEventListener("click", Descifrar);
-buttonNext.addEventListener("click",() => {
-mostrando1.style.display="none";
-ocultado1.style.display="block";
+buttonNext.addEventListener("click", () => {
+  mostrando1.style.display = "none";
+  ocultado1.style.display = "block";
 })
-buttonReturn.addEventListener("click",() => {
-ocultado1.style.display="none";
-mostrando1.style.display="block";
+buttonReturn.addEventListener("click", () => {
+  ocultado1.style.display = "none";
+  mostrando1.style.display = "block";
 })
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,15 +13,11 @@ describe('cipher', () => {
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
       assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG")
     });
-<<<<<<< HEAD
-    it('deberia retornar "ABC123" para "BCD123" con offset 1',()=>{
-      assert.equal(cipher.encode(1,"ABC123"),"BCD123")
-    } )
-=======
-    it('deberia retornar "BCD" para "ABC123" con offset 1', ()=>{
-      assert.equal(cipher.encode(1,"ABC123"),"BCD")
-    } )           
->>>>>>> 59987d1e4f8c8a920ca441aa484397b742ee75a7
+
+    it('deberia retornar "BCD" para "ABC123" con offset 1', () => {
+      assert.equal(cipher.encode(1, "ABC123"), "BCD")
+    })
+
   });
 
   describe('cipher.decode', () => {
@@ -30,23 +26,13 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-<<<<<<< HEAD
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+
+    it('debería retornar"ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
       assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     });
-    it('deberia retornar "BCD@" para "ABC@" con offset 1',()=>{
-=======
-    it('debería retornar"HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
-      assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    });
-    it('deberia retornar "ZAB" para "ABC@" con offset 1',()=>{
->>>>>>> 59987d1e4f8c8a920ca441aa484397b742ee75a7
-      assert.equal(cipher.decode(1,"ABC@"),"ZAB")
-    } )
+    it('deberia retornar "ZAB" para "ABC@" con offset 1', () => {
+      assert.equal(cipher.decode(1, "ABC@"), "ZAB")
+    })
   });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 59987d1e4f8c8a920ca441aa484397b742ee75a7
 });
